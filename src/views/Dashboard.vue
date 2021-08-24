972e4d6c8081f62b6435e61795734cb27afe305d
okon ifiok friday <wisdominterconnect@gmail.com>
2021-08-24 21:10:47 +0100

    working on change

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,9 +3,32 @@
         <div class="col-4 col-md-2 p-md-1 nav-sticky">
             <div class="row d-flex mt-4">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span class="mb-1"> <img src="../assets/store.svg" class="icon1" alt=""> </span>
+                    <span class="mb-2"> <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_dd)">
+<rect x="5" y="3" width="28" height="28" rx="14" fill="white"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14 16.5V21H24V16.5C24.718 16.5 25.398 16.332 26 16.032V24C26 24.2652 25.8946 24.5196 25.7071 24.7071C25.5196 24.8946 25.2652 25 25 25H13C12.7348 25 12.4804 24.8946 12.2929 24.7071C12.1054 24.5196 12 24.2652 12 24V16.032C12.602 16.332 13.282 16.5 14 16.5ZM11 12L12.703 9.445C12.7943 9.30808 12.9181 9.19583 13.0632 9.11821C13.2084 9.04058 13.3704 8.99998 13.535 9H24.465C24.6296 8.99998 24.7916 9.04058 24.9368 9.11821C25.0819 9.19583 25.2057 9.30808 25.297 9.445L27 12C27.0001 12.6445 26.7927 13.272 26.4084 13.7894C26.0241 14.3068 25.4834 14.6867 24.8664 14.8729C24.2493 15.059 23.5887 15.0415 22.9824 14.8228C22.3761 14.6042 21.8563 14.1961 21.5 13.659C20.963 14.467 20.043 15 19 15C17.957 15 17.037 14.467 16.5 13.659C16.1437 14.1961 15.6239 14.6042 15.0176 14.8228C14.4113 15.0415 13.7507 15.059 13.1336 14.8729C12.5166 14.6867 11.9759 14.3068 11.5916 13.7894C11.2073 13.272 10.9999 12.6445 11 12Z" fill="#A3ACB9"/>
+</g>
+<defs>
+<filter id="filter0_dd" x="0" y="0" width="38" height="38" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="2"/>
+<feGaussianBlur stdDeviation="2.5"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0.235294 0 0 0 0 0.258824 0 0 0 0 0.341176 0 0 0 0.08 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="1"/>
+<feGaussianBlur stdDeviation="0.5"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
+<feBlend mode="normal" in2="effect1_dropShadow" result="effect2_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+</span>
                 </div>
-                <div class="col-md-8  col-7 d-flex justify-content-start">
+                <div class="col-md-8 mt-2  col-7 d-flex justify-content-start">
                     <p class="storeFont">Store name</p>
                     <span class=""><i class=" pi pi-angle-down"></i></span>
                 </div>
@@ -13,17 +36,23 @@
             </div>
              <div class="row d-flex mt-5 mb-4">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span> <img src="../assets/home.svg" class="image1" alt=""> </span>
+                    <span> <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.5 14.299V10.894C8.49948 10.815 8.48334 10.7368 8.45252 10.664C8.42169 10.5912 8.37679 10.5252 8.32038 10.4698C8.26397 10.4144 8.19718 10.3708 8.12384 10.3413C8.05049 10.3118 7.97204 10.2971 7.893 10.298H6.107C5.94767 10.2972 5.79447 10.3594 5.68078 10.471C5.56709 10.5826 5.50211 10.7347 5.5 10.894V14.299H1.5C1.36877 14.2993 1.23878 14.2736 1.11747 14.2236C0.996154 14.1736 0.885899 14.1001 0.793014 14.0074C0.700129 13.9147 0.626438 13.8046 0.576159 13.6834C0.52588 13.5622 0.5 13.4322 0.5 13.301V6.296C0.5 5.915 0.718 5.567 1.06 5.4L7.02 0.5L12.916 5.389C13.0901 5.46823 13.2377 5.59587 13.3413 5.75669C13.4448 5.91751 13.4999 6.10473 13.5 6.296V13.301C13.5 13.4321 13.4742 13.562 13.4239 13.6831C13.3737 13.8043 13.3001 13.9144 13.2073 14.007C13.1146 14.0997 13.0044 14.1732 12.8832 14.2233C12.762 14.2734 12.6321 14.2991 12.501 14.299H8.5ZM7 8.099C7.17072 8.099 7.33976 8.06537 7.49749 8.00004C7.65521 7.93471 7.79852 7.83895 7.91924 7.71824C8.03996 7.59752 8.13571 7.45421 8.20104 7.29649C8.26637 7.13877 8.3 6.96972 8.3 6.799C8.3 6.62828 8.26637 6.45923 8.20104 6.30151C8.13571 6.14379 8.03996 6.00048 7.91924 5.87976C7.79852 5.75905 7.65521 5.66329 7.49749 5.59796C7.33976 5.53263 7.17072 5.499 7 5.499C6.65522 5.499 6.32456 5.63596 6.08076 5.87976C5.83696 6.12356 5.7 6.45422 5.7 6.799C5.7 7.14378 5.83696 7.47444 6.08076 7.71824C6.32456 7.96204 6.65522 8.099 7 8.099Z" fill="#A3ACB9"/>
+</svg>
+ </span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 mt-1 col-7 d-flex justify-content-start">
                     <p class="sidebarfont">Home</p>
                 </div>
             </div>
             <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span><img src="../assets/paymentimg.svg" alt=""></span>
+                    <span><svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13 8.5H1C0.448 8.5 0 8.087 0 7.577V1.423C0 0.913 0.448 0.5 1 0.5H13C13.552 0.5 14 0.913 14 1.423V7.577C14 8.087 13.552 8.5 13 8.5ZM7.006 2.5C6.74296 2.49961 6.48242 2.55103 6.23926 2.65132C5.99609 2.75162 5.77506 2.89883 5.58879 3.08454C5.40251 3.27026 5.25464 3.49085 5.15362 3.73371C5.05259 3.97658 5.00039 4.23696 5 4.5C4.99961 4.76304 5.05103 5.02358 5.15132 5.26674C5.25162 5.50991 5.39883 5.73094 5.58454 5.91721C5.77026 6.10349 5.99085 6.25136 6.23371 6.35238C6.47658 6.45341 6.73696 6.50561 7 6.506C7.53123 6.5068 8.04102 6.29653 8.41721 5.92146C8.79341 5.54638 9.0052 5.03723 9.006 4.506C9.00679 3.97477 8.79653 3.46498 8.42146 3.08879C8.04638 2.71259 7.53723 2.5008 7.006 2.5Z" fill="#5469D4"/>
+</svg>
+</span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 col-7 mt-1 d-flex justify-content-start">
                     <p class="mb-3 dropDown">Payments </p>
                 </div>
                 <div class="row m-0 p-0 d-flex">
@@ -77,65 +106,91 @@
                 </div>
                  <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span> <img src="../assets/balanceimg.svg" class="image1" alt=""> </span>
+                    <span> <svg width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.25078 7.12495V1.528C6.25078 1.39791 6.22508 1.26911 6.17515 1.14899C6.12522 1.02886 6.05204 0.919792 5.95983 0.828038C5.86761 0.736283 5.75817 0.663655 5.6378 0.614326C5.51743 0.564997 5.3885 0.53994 5.25842 0.540593C4.71063 0.540593 4.26605 0.99212 4.26605 1.528V7.11106L2.45398 5.299C2.362 5.20633 2.25262 5.13275 2.13211 5.08248C2.01161 5.0322 1.88237 5.00623 1.7518 5.00604C1.62123 5.00586 1.49191 5.03147 1.37127 5.0814C1.25063 5.13134 1.14104 5.20461 1.04879 5.29701C0.955787 5.38892 0.881983 5.4984 0.831673 5.61909C0.781363 5.73978 0.75555 5.86927 0.755734 6.00002C0.755919 6.13078 0.782098 6.26019 0.832749 6.38074C0.8834 6.50129 0.957512 6.61056 1.05078 6.7022L4.55483 10.2063C4.75131 10.4037 5.00933 10.501 5.26536 10.5C5.39755 10.5009 5.52858 10.4754 5.65077 10.4249C5.77295 10.3745 5.88383 10.3001 5.97689 10.2063L9.48094 6.7022C9.87789 6.30526 9.87094 5.68404 9.48293 5.29701C9.39068 5.20461 9.28109 5.13134 9.16045 5.0814C9.0398 5.03147 8.91048 5.00586 8.77992 5.00604C8.64935 5.00623 8.5201 5.0322 8.3996 5.08248C8.2791 5.13275 8.16972 5.20633 8.07773 5.299L6.25178 7.12495H6.25078Z" fill="#4F566B"/>
+</svg>
+ </span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 col-7 mt-1 d-flex justify-content-start">
                     <p class="dropdownfont">Balances</p>
                 </div>
             </div>
                <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span><img src="../assets/customerimg.svg" alt=""></span>
+                    <span><svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.445 14.361C12.413 13.385 10.362 12.72 8 12.72C5.638 12.72 3.587 13.385 2.555 14.361C1.37743 13.267 0.55669 11.8437 0.199753 10.2765C-0.157184 8.70932 -0.0337699 7.07095 0.553912 5.57492C1.14159 4.07889 2.16629 2.79457 3.49446 1.88934C4.82263 0.98411 6.39268 0.499947 8 0.499947C9.60732 0.499947 11.1774 0.98411 12.5055 1.88934C13.8337 2.79457 14.8584 4.07889 15.4461 5.57492C16.0338 7.07095 16.1572 8.70932 15.8002 10.2765C15.4433 11.8437 14.6226 13.267 13.445 14.361ZM8 9.731C8.81607 9.731 9.59872 9.40682 10.1758 8.82977C10.7528 8.25272 11.077 7.47007 11.077 6.654C11.077 5.83793 10.7528 5.05528 10.1758 4.47823C9.59872 3.90119 8.81607 3.577 8 3.577C7.18393 3.577 6.40128 3.90119 5.82423 4.47823C5.24718 5.05528 4.923 5.83793 4.923 6.654C4.923 7.47007 5.24718 8.25272 5.82423 8.82977C6.40128 9.40682 7.18393 9.731 8 9.731Z" fill="#4F566B"/>
+</svg></span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 col-7 mt-1 d-flex justify-content-start">
                     <p class="dropdownfont">Customers</p>
                 </div>
             </div>
              <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span> <img src="../assets/accountimg.svg" alt=""></span>
+                    <span>
+                        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8 16.5C5.87827 16.5 3.84344 15.6571 2.34315 14.1569C0.842855 12.6566 0 10.6217 0 8.5C0 6.37827 0.842855 4.34344 2.34315 2.84315C3.84344 1.34285 5.87827 0.5 8 0.5C8.45489 0.5 8.89978 0.546988 9.33367 0.61997V5.49875H10.9993V7.16633H15.88C15.953 7.60122 16 8.04611 16 8.5C16 10.6217 15.1571 12.6566 13.6569 14.1569C12.1566 15.6571 10.1217 16.5 8 16.5ZM9.9915 10.4915H6.34641C6.25626 10.4915 6.1698 10.4557 6.10606 10.3919C6.04231 10.3282 6.0065 10.2417 6.0065 10.1516V6.5125H3.56711C3.52249 6.5125 3.47832 6.52133 3.43714 6.53849C3.39596 6.55565 3.35858 6.58079 3.32717 6.61247L1.09773 8.84191C1.18082 10.5565 1.89972 12.1785 3.11416 13.3917C4.3286 14.6048 5.95145 15.322 7.66608 15.4033L9.89253 13.1758C9.95551 13.1128 9.9925 13.0259 9.9925 12.9359V10.4895L9.9915 10.4915Z" fill="#A3ACB9"/>
+</svg>
+
+
+</span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 col-7 mt-1 d-flex justify-content-start">
                     <p class="dropdownfont">Connected</p>
                 </div>
             </div>
              <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span><img src="../assets/product.img.svg" class="image2" alt=""></span>
+                    <span><svg width="13" height="15" viewBox="0 0 13 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.587 2.87863H11.917L12.259 3.03863C12.596 3.19863 13 3.67863 13 4.12863V11.1946C13 11.5606 12.596 11.8946 12.259 12.0556L6.53302 14.8256C6.41002 14.8836 6.27502 14.9126 6.00002 14.9126V8.89563H0.660025V3.56263L7.70403 0.969627L7.82102 0.859627L10.3 2.06063L9.96402 2.89563L11.588 2.87863H11.587Z" fill="#4F566B"/>
+</svg>
+</span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 mt-1 col-7 d-flex justify-content-start">
                     <p class="dropdownfont">Products</p>
                 </div>
             </div>
              <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span><img src="../assets/reportimg.svg" class="image1" alt=""> </span>
+                    <span><svg width="14" height="17" viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 9.5H1C1.26522 9.5 1.51957 9.39464 1.70711 9.20711C1.89464 9.01957 2 8.76522 2 8.5C2 8.23478 1.89464 7.98043 1.70711 7.79289C1.51957 7.60536 1.26522 7.5 1 7.5H0V5.5H1C1.26522 5.5 1.51957 5.39464 1.70711 5.20711C1.89464 5.01957 2 4.76522 2 4.5C2 4.23478 1.89464 3.98043 1.70711 3.79289C1.51957 3.60536 1.26522 3.5 1 3.5H0V2.5C0 1.96957 0.210714 1.46086 0.585786 1.08579C0.960859 0.710714 1.46957 0.5 2 0.5L12 0.5C12.5304 0.5 13.0391 0.710714 13.4142 1.08579C13.7893 1.46086 14 1.96957 14 2.5V14.5C14 15.0304 13.7893 15.5391 13.4142 15.9142C13.0391 16.2893 12.5304 16.5 12 16.5H2C1.46957 16.5 0.960859 16.2893 0.585786 15.9142C0.210714 15.5391 0 15.0304 0 14.5V13.5H1C1.26522 13.5 1.51957 13.3946 1.70711 13.2071C1.89464 13.0196 2 12.7652 2 12.5C2 12.2348 1.89464 11.9804 1.70711 11.7929C1.51957 11.6054 1.26522 11.5 1 11.5H0V9.5Z" fill="#A3ACB9"/>
+</svg>
+ </span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 mt-1 col-7 d-flex justify-content-start">
                     <p class="dropdownfont">Reports</p>
                 </div>
             </div>
              <div class="row d-flex mt-4">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                   <span><img src="../assets/developerimg.svg" alt=""></span>
+                   <span><svg width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.1 8.502C11.1001 8.22622 11.0459 7.95312 10.9405 7.69829C10.8351 7.44345 10.6805 7.21188 10.4856 7.01678C10.2907 6.82169 10.0593 6.66689 9.80456 6.56124C9.54983 6.45558 9.27678 6.40113 9.001 6.401C8.72522 6.40087 8.45212 6.45506 8.19729 6.56047C7.94245 6.66588 7.71088 6.82046 7.51578 7.01537C7.32069 7.21028 7.16589 7.44171 7.06024 7.69644C6.95458 7.95117 6.90013 8.22422 6.9 8.5H1C0.448 8.5 0 8.087 0 7.577V1.423C0 0.913 0.448 0.5 1 0.5H13.002C13.555 0.5 14.002 0.913 14.002 1.423V8.503L11.1 8.502ZM7.006 2.5C6.47477 2.4992 5.96498 2.70947 5.58879 3.08454C5.21259 3.45962 5.0008 3.96877 5 4.5C4.9992 5.03123 5.20947 5.54102 5.58454 5.91721C5.95962 6.29341 6.46877 6.5052 7 6.506C7.26304 6.50639 7.52358 6.45497 7.76674 6.35468C8.00991 6.25438 8.23094 6.10717 8.41721 5.92146C8.60349 5.73574 8.75136 5.51515 8.85238 5.27229C8.95341 5.02942 9.00561 4.76904 9.006 4.506C9.00639 4.24296 8.95498 3.98242 8.85468 3.73926C8.75438 3.49609 8.60717 3.27506 8.42146 3.08879C8.23574 2.90251 8.01515 2.75464 7.77229 2.65362C7.52942 2.55259 7.26904 2.50039 7.006 2.5Z" fill="#A3ACB9"/>
+</svg>
+</span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 mt-1 col-7 d-flex justify-content-start">
                     <p class="dropdownfont">Developers</p>
                 </div>
             </div>
              <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span> <i class="pi pi-eye"></i> </span>
+                    <span> <svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="1" width="25" height="15" rx="7.5" fill="#E3E8EE" stroke="#D9DCE1"/>
+</svg>
+ </span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md-8 mt-1 col-7 d-flex justify-content-start">
                     <p class="dropdownfont">View test data</p>
                 </div>
             </div>
              <div class="row d-flex">
                 <div class="col-md-4 col-5 d-flex justify-content-end">
-                    <span><i class="pi pi-cog"></i></span>
+                    <span><svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M13.197 6.517C12.967 6.517 11.59 6.517 11.017 5.822C11.017 5.707 10.902 5.707 10.902 5.591C10.787 4.781 11.82 3.74 11.934 3.624C12.164 3.393 12.164 3.045 12.049 2.93C11.934 2.814 11.475 2.351 11.361 2.12C11.246 1.888 10.901 2.004 10.672 2.236C10.442 2.466 9.525 3.393 8.722 3.276C8.607 3.276 8.607 3.161 8.492 3.161C7.803 2.698 7.803 1.31 7.803 1.079C7.803 0.731 7.689 0.5 7.459 0.5H6.311C6.082 0.5 5.967 0.731 5.967 1.079C5.967 1.309 5.967 2.699 5.279 3.277C5.164 3.277 5.164 3.393 5.049 3.393C4.246 3.393 3.328 2.467 3.099 2.236C2.869 2.004 2.525 2.004 2.409 2.12C2.295 2.351 1.836 2.814 1.607 2.93C1.492 3.045 1.492 3.393 1.721 3.624C1.951 3.855 2.869 4.781 2.754 5.591C2.754 5.707 2.639 5.707 2.639 5.822C2.18 6.517 0.803 6.517 0.574 6.517C0.23 6.517 0 6.632 0 6.864V7.905C0 8.136 0.23 8.368 0.574 8.368C0.804 8.368 2.18 8.368 2.754 9.062C2.754 9.178 2.869 9.178 2.869 9.293C2.984 10.103 1.951 11.145 1.836 11.26C1.606 11.492 1.606 11.839 1.721 11.955C1.836 12.07 2.295 12.533 2.41 12.765C2.525 12.88 2.869 12.88 3.098 12.649C3.328 12.417 4.246 11.492 5.049 11.607C5.164 11.607 5.164 11.723 5.279 11.723C5.967 12.302 5.967 13.574 5.967 13.921C5.967 14.153 6.197 14.5 6.427 14.5H7.459C7.689 14.5 7.918 14.269 7.918 13.921C7.918 13.691 7.918 12.301 8.607 11.723C8.721 11.723 8.721 11.607 8.837 11.607C9.639 11.492 10.672 12.533 10.787 12.649C11.017 12.88 11.361 12.88 11.475 12.764L12.279 11.954C12.393 11.839 12.393 11.492 12.164 11.26C11.934 11.029 11.016 10.103 11.131 9.293C11.131 9.178 11.246 9.178 11.246 9.062C11.82 8.368 13.082 8.368 13.426 8.368C13.656 8.368 14 8.136 14 7.905V6.864C13.77 6.632 13.541 6.517 13.197 6.517ZM6.885 9.987C5.508 9.987 4.361 8.831 4.361 7.442C4.361 6.054 5.508 4.897 6.885 4.897C8.262 4.897 9.41 6.054 9.41 7.442C9.41 8.831 8.262 9.988 6.885 9.988V9.987Z" fill="#4F566B"/>
+</svg>
+</span>
                 </div>
-                <div class="col-md-8 col-7 d-flex justify-content-start">
+                <div class="col-md- mt-1 col-7 d-flex justify-content-start">
                     <p class="dropdownfont">Setting</p>
                 </div>
             </div>
@@ -207,10 +262,9 @@
                    <hr>
                </div>
                <!--- table-->
-                <div class="row mt-0 px-5">
+                <div class="row mt-0 px-5 d-flex justify-content-between">
                    <div class="col-md-7">
-                       <div class="row d-flex justify-content-evenly">
-                           <!-- <div class="col-md-1 d-flex justify-content-start"><input type="checkbox" name="" id=""></div> -->
+                       <div class="row d-flex justify-content-between">
                            <div class="col-md-1"><input type="checkbox" name="" id=""></div>
                            <div class="col-md-2 amount">Amount</div>
                            <div class="col-md-1"></div>
@@ -237,11 +291,11 @@
                <!--Populate Data-->
                <div class="row mt-0  px-5" v-for="product in items" :key="product.id" >
                    <div class="col-md-7">
-                       <div class="row d-flex justify-content-evenly">
+                       <div class="row justify-content-between">
                            <div class="col-md-1"><input type="checkbox" name="" id=""></div>
                            <div class="col-md-1 amount1">{{product.amount}}</div>
                            <div class="col-md-1  succeeded">{{product.succeeded}} <i class="pi pi-check"></i> </div>
-                           <div class="col-md-4 description">{{product.description}}</div>
+                           <div class="col-md-3 description">{{product.description}}</div>
                        </div>
 
                    </div>
